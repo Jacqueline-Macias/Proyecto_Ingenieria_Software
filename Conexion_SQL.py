d1ae4d9944efd56fcb6d734887999b15005f41fd
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-12-03 16:38:35 -0600

    feat: agregar clase provider

--- Conexion_SQL.py
+++ Conexion_SQL.py
@@ -2,13 +2,45 @@ import mysql.connector
 
 class connect_foodsoft:
     def __init__(self, Host, username, PassWord, Port, database_name):
-        Sql = mysql.connector.connect(host=Host,
+        self.connection = mysql.connector.connect(host=Host,
                                       user=username,
                                       passwd=PassWord,
                                       port=Port,
                                       database=database_name)
+        self.cursor = self.connection.cursor()
+
+class provider(connect_foodsoft):
+
+    def Add(self,id,name,phone,email,type,rfc):
+        try:
+            sql = "INSERT INTO proveedor(id_proveedor,nombre,telefono,correo,tipo_cadena,rfc) VALUES('{}','{}','{}','{}','{}','{}')".format(id,name,phone,email,type,rfc)
+            self.cursor.execute(sql)
+            self.connection.commit()
+            self.cursor.close()
+            self.connection.close()
+        except Exception as e:
+            raise
+    
+    def modifier(self,id,name,phone,email,type,rfc):
+        sql = "UPDATE proveedor SET nombre='{}',telefono='{}',correo='{}',tipo_cadena='{}',rfc='{}' WHERE id_proveedor = '{}'".format(
+            name, phone, email, type, rfc,id)
+        self.cursor.execute(sql)
+        self.connection.commit()
+        self.cursor.close()
+        self.connection.close()
+    
+    def select_provider(self,id):
+        sql = "SELECT * FROM proveedor WHERE id_proveedor = '{}'".format(id)
+        self.cursor.execute(sql)
+        date = self.cursor.fetchone()
+        self.cursor.close()
+        self.connection.close()
+
+        return date
+
+
         
-        
+            
 
 
 
