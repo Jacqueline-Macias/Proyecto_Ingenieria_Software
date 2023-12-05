2de9d487d21ab7ddaa607f227f0575b8324ea695
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-12-05 15:00:16 -0600

    feat: agregar clase supply

--- Conexion_SQL.py
+++ Conexion_SQL.py
@@ -45,10 +45,63 @@ class provider(connect_foodsoft):
         finally:
             cursor.close()
 
+    def select(self,search):
+        try:
+            cursor = self.connection.cursor()
+            sql = f"SELECT {search} FROM proveedor"
+            cursor.execute(sql)
+            date = cursor.fetchall()
+        except Exception as Ex:
+            print(Ex)
+        finally:
+            cursor.close()
+            list_date = []
+            for count in date:
+                list_date.append(count[0])
+            return list_date
+
+
     def select_all(self):
         try:
             cursor = self.connection.cursor()
             sql = "SELECT * FROM proveedor"
+            cursor.execute(sql)
+            date = cursor.fetchall()
+        except Exception as Ex:
+            print(Ex)
+        finally:
+            cursor.close()
+            return date
+
+
+class supply(connect_foodsoft):
+
+    def Add(self, id_provider, name, min, max, price):
+        try:
+            cursor = self.connection.cursor()
+            sql = "INSERT INTO insumo(nombre, stock_min, stock_max, precio, id_proveedor) VALUES('{}','{}','{}','{}','{}')".format(name,min,max,price,id_provider)
+            cursor.execute(sql)
+            self.connection.commit()
+        except Exception as Ex:
+            print(Ex)
+        finally:
+            cursor.close()
+
+    def modifier(self,id,name,min,max,price):
+        try:
+            cursor = self.connection.cursor()
+            sql = "UPDATE insumo SET stock_min = '{}', stock_max = '{}', precio = '{}', id_proveedor = '{} WHERE nombre = '{}'".format(min,max,price,id,name)
+            cursor.execute(sql)
+            self.connection.commit()
+        except Exception as Ex:
+            print(Ex)
+        finally:
+            cursor.close()
+
+    def select_all(self):
+        try:
+            cursor = self.connection.cursor()
+            sql = "SELECT * FROM insumo"
             cursor.execute(sql)
             date = cursor.fetchall()
         except Exception as Ex:
